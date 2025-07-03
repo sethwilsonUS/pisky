bc73799aa821533af8ed40e170c5550fc3b3e6fa
Seth Wilson <sethhwilson@gmail.com>
2025-07-02 19:20:59 -0500

    add initial migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250703_002027 from './20250703_002027';
+
+export const migrations = [
+  {
+    up: migration_20250703_002027.up,
+    down: migration_20250703_002027.down,
+    name: '20250703_002027'
+  },
+];
